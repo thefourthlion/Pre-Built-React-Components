fe01cf7b2ac3cf681b8fccd5414ad135e7e1a92e
TheFourthLion <46293419+thefourthlion@users.noreply.github.com>
2022-09-30 19:34:09 -0700

    Add files via upload

---
+++ React-Components/OutlineBtn/Buttons/OutlineBtn-Animation/OutlineBtn.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./OutlineBtn.css";
+const OutlineBtn = () => {
+  return (
+    <div className="container">
+      <button className="outline-btn btn-5">
+        <span>Hello World</span>
+      </button>
+    </div>
+  );
+};
+
+export default OutlineBtn;
